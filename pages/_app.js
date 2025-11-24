2c2b985492928c0cc25a352e7c4535c000138955
d87skg <jmhbldg@gmail.com>
2025-11-24 21:43:01 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,11 @@
 import "../styles/globals.css";
-import "@web3modal/react/dist/css/reset.css";
 import { Web3Modal } from "@web3modal/react";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { base } from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
-import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
-
-const { chains, provider } = configureChains(
-  [base],
-  [
-    jsonRpcProvider({
-      rpc: (chain) => ({ http: "https://mainnet.base.org" }),
-    }),
-    publicProvider(),
-  ]
-);
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  provider,
-});
-
-const ethereumClient = new Web3Modal(
-  { projectId: "YOUR_PROJECT_ID" }, // 随便填，后面用不到
-  wagmiClient
-);
+import { WagmiConfig } from "wagmi";
+import { wagmiConfig } from "../wagmi";
 
 export default function App({ Component, pageProps }) {
   return (
-    <WagmiConfig client={wagmiClient}>
+    <WagmiConfig config={wagmiConfig}>
       <Component {...pageProps} />
     </WagmiConfig>
   );
